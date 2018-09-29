1f694845f715940def11f3f6d6dcc77ef8c43c7f
liqinghui <liqinghui@autohome.com.cn>
2018-09-29 14:17:34 +0800

    带单位

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,10 @@ var app = getApp()
  var i=0
 Page({
   data: {
+    //金币单位索引
+    jbdwIndex:0,
+    zzdwIndex:0,
+    dw:["无","K", "M", "B", "T", "aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj", "kk", "ll", "mm", "nn", "oo", "pp", "qq", "rr", "ss", "tt", "uu", "vv", "ww", "xx", "yy", "zz"],
     j:0.04,
     userInfo: {}
   },
@@ -13,30 +17,87 @@ Page({
       url: '../logs/logs'
     })
   },
+  
   onLoad: function () {
     console.log('onLoad')
+    var jbdw = wx.getStorageSync('jbdw');
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       //更新数据
       that.setData({
+        jbdwIndex:jbdw,
         userInfo:userInfo
       })
     })
   },
- 
-   changeName: function(e) {
-     
+  zzbindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      zhognzi: 0.04 * Math.pow(e.detail.value, 0.3787)
+      zzdwIndex: e.detail.value
+    })
+    wx.setStorage({
+      key: "zzdw",
+      data: e.detail.value
+    })
+    this.jb2zz();
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      jbdwIndex: e.detail.value
+    })
+    wx.setStorage({
+      key: "jbdw",
+      data: e.detail.value
+    })
+    this.jb2zz();
+  },
+  //金币改变
+   changeName: function(e) {
+     const value = e.detail.value;
+     this.setData({
+       jinbi: value,
+     })
+     this.jb2zz();
+  },
+  //金币转换豆子
+  jb2zz:function(){
+    let jb=this.data.jinbi;
+    let dw = Math.pow(1000, this.data.jbdwIndex);
+    let dz = 0.04 * Math.pow(jb * dw, 0.3787);
+    let dzdw = this.jsdw(dz,0);
+    this.setData({
+      zhognzi: dzdw.value.toFixed(2)+""+dzdw.dw,
     })
   },
+  
+  //单位换算
+  jsdw:function(v,i){
+      if(v<1000){
+        
+        return { value: v, dw: this.data.dw[i]};
+      }
+      return this.jsdw(v/1000,++i);
+  },
+  //豆子变动
   changeName2: function (e) {
-   // y = ax ^ b  => x = (1 / a) ^ (1 / b)y ^ (1 / b)
+    let value = e.detail.value;
+    this.setData({
+      zzzhongzi: value,
+    })
+    this.zz2jb();
+  },
+  //种子转换金币
+  zz2jb: function () {
+    let zz = this.data.zzzhongzi;
+    let dw = Math.pow(1000, this.data.zzdwIndex);
+    let jinbi = Math.pow((1 / 0.04), 1 / 0.3787) * Math.pow(zz * dw, 1 / 0.3787)
+    let jbdw = this.jsdw(jinbi, 0);
 
     this.setData({
-      jinbi: Math.pow((1 / 0.04), 1/0.3787) * Math.pow(e.detail.value, 1/0.3787)
+      jinbi: jbdw.value.toFixed(2) + "" + jbdw.dw,
     })
-  }
+  },
  
 })
